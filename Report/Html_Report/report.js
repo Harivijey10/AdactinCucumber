46c20fee704ef489575cc6f144546d39c261513e
Harivijey10 <harivijeymurugesan@gmail.com>
2024-05-01 15:42:09 +0530

    code updated successfully

---
+++ Report/Html_Report/report.js
@@ -0,0 +1,325 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Adactin.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Hotel Booking In Adactin Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "User Able To Login The Web Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-able-to-login-the-web-application",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "user Launch The Adactin Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "user Enter The User Name In The Username Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "user Enter The Password In The Password Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "user Should Click The Login Button And User Should Navigate To Search Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Launch_The_Adactin_Application()"
+});
+formatter.result({
+  "duration": 8891686400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Enter_The_User_Name_In_The_Username_Field()"
+});
+formatter.result({
+  "duration": 1301184000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Enter_The_Password_In_The_Password_Field()"
+});
+formatter.result({
+  "duration": 693686700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Should_Click_The_Login_Button_And_User_Should_Navigate_To_Search_Hotel_Page()"
+});
+formatter.result({
+  "duration": 3002436300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 9,
+  "name": "User Should Be Able to Search Hotel",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-should-be-able-to-search-hotel",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 10,
+  "name": "user Able To Select Location In The Loation Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "user Able To Select Hotel In The Hotels Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "user Able To Select Room Type In The Room Type Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "user Able To Select Number Of Rooms In The Number Of Rooms Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "user Able To Select Number Of Adults In Adults Per Room Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "user Able To Select Number Of Children In Children Per Room Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "user Should Click Search Button And User Should Navigate To Select Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Location_In_The_Loation_Field()"
+});
+formatter.result({
+  "duration": 3547716700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Hotel_In_The_Hotels_Field()"
+});
+formatter.result({
+  "duration": 2216801500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Room_Type_In_The_Room_Type_Field()"
+});
+formatter.result({
+  "duration": 2159275000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Number_Of_Rooms_In_The_Number_Of_Rooms_Field()"
+});
+formatter.result({
+  "duration": 1695741300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Number_Of_Adults_In_Adults_Per_Room_Field()"
+});
+formatter.result({
+  "duration": 1815960300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Number_Of_Children_In_Children_Per_Room_Field()"
+});
+formatter.result({
+  "duration": 1434861200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Should_Click_Search_Button_And_User_Should_Navigate_To_Select_Hotel_Page()"
+});
+formatter.result({
+  "duration": 1659399900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "User Should Be Able To Select Hotel",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-should-be-able-to-select-hotel",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 19,
+  "name": "user Able To Click The Radio Button In The Select Hotel Page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 20,
+  "name": "user Should Click Continue Button And User Should Navigate To Book A Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Click_The_Radio_Button_In_The_Select_Hotel_Page()"
+});
+formatter.result({
+  "duration": 671601900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Should_Click_Continue_Button_And_User_Should_Navigate_To_Book_A_Hotel_Page()"
+});
+formatter.result({
+  "duration": 703344500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 22,
+  "name": "User Should Be Able To Book A Hotel",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-should-be-able-to-book-a-hotel",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 23,
+  "name": "user Able To Enter Name In The First Name Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 24,
+  "name": "user Able To Enter Name In The Last Name Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "user Able To Enter Address In The Billing Address Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "user Able To Enter card Number In Credit Card Number Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "user Able To Select Card Type In Credit Card Type Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "user Able To Select Month And Year In The Expiry Date Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "user Able To Enter Number In The CVV Number Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "user Should Click The Book Now Button And User Should Navigate To Booking Confirmation Page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 31,
+  "name": "user Should Click My Itinerary Button And User Should Navigate To Booked Itinerary",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Enter_Name_In_The_First_Name_Field()"
+});
+formatter.result({
+  "duration": 2070379200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Enter_Name_In_The_Last_Name_Field()"
+});
+formatter.result({
+  "duration": 834777900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Enter_Address_In_The_Billing_Address_Field()"
+});
+formatter.result({
+  "duration": 1182381100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Enter_card_Number_In_Credit_Card_Number_Field()"
+});
+formatter.result({
+  "duration": 909931600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Card_Type_In_Credit_Card_Type_Field()"
+});
+formatter.result({
+  "duration": 1462619700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Select_Month_And_Year_In_The_Expiry_Date_Field()"
+});
+formatter.result({
+  "duration": 4791468800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Able_To_Enter_Number_In_The_CVV_Number_Field()"
+});
+formatter.result({
+  "duration": 691503500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Should_Click_The_Book_Now_Button_And_User_Should_Navigate_To_Booking_Confirmation_Page()"
+});
+formatter.result({
+  "duration": 451652500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Should_Click_My_Itinerary_Button_And_User_Should_Navigate_To_Booked_Itinerary()"
+});
+formatter.result({
+  "duration": 25304565100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 33,
+  "name": "User Should Be Able To Log Out The Application",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application;user-should-be-able-to-log-out-the-application",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 34,
+  "name": "user Should Scroll Down And Click Log Out Button And User Should Log Out From The Application",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Should_Scroll_Down_And_Click_Log_Out_Button_And_User_Should_Log_Out_From_The_Application()"
+});
+formatter.result({
+  "duration": 3008793000,
+  "status": "passed"
+});
+});
--- src/test/java/com/adactin/runner/Test_Runner.java
+++ src/test/java/com/adactin/runner/Test_Runner.java
@@ -11,10 +11,11 @@ import cucumber.api.junit.Cucumber;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\java\\com\\adactin\\feature",
+@CucumberOptions(features = "src//test//java//com//adactin//feature",
 glue = "com.adactin.stepdefinition",
 dryRun = false,
-strict = true)
+strict = true, plugin = { "html:Report/Html_Report", "pretty", "json:Report/Json_Report.json",
+"com.cucumber.listener.ExtentCucumberFormatter:Report/Extent_Report.html"})
 public class Test_Runner {
 	public static WebDriver driver;
 	
